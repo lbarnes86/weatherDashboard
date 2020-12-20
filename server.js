8a91b0f5025e7d4cf4136a60c0afc57518aff82f
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-19 23:26:59 -0600

    updated server.js

--- server.js
+++ server.js
@@ -30,3 +30,34 @@
 
             
         }
+
+        function uviCalc(value) {
+            var decorator = '';
+
+            if (value >= 0 && value < 3) {
+                
+                decorator = 'bg-success';
+            } else if (value >= 3 && value < 5) {
+                
+                decorator = 'bg-warning';
+            } else if (value >= 5 && value < 7) {
+                
+                decorator = 'bg-dark';
+            } else if (value >= 7 && value < 11) {
+                
+                decorator = 'bg-danger';
+            } else if (value >= 11) {
+                
+                decorator = 'bg-light';
+            }
+            return decorator;
+        };
+        function updateWeatherInfoView(weatherInfo) {
+            var icon_href = `https://openweathermap.org/img/wn/${weatherInfo.current.weather[0].icon}@2x.png`;
+
+            $("#icon").attr("src", icon_href)
+            $("#city").html(weatherInfo.current.name);
+            $("#date").html(moment().format('MM/DD/YYYY'));
+            $("#temp").html(weatherInfo.forecast.current.temp);
+            $("#humidity").html(weatherInfo.forecast.current.humidity);
+            $("#windSpeed").html(weatherInfo.forecast.current.wind_speed);
